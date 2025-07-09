ece1459da8d39743ab10d9411d90ad2c4f5942c4
5600X2 <willbecat27@gmail.com>
2025-07-09 12:18:02 +0900

    복소수 구조체 수정

--- workspace/complex.c
+++ workspace/complex.c
@@ -6,17 +6,17 @@ typedef struct {
 } Complex;
 
 // Add two complex numbers
-Complex complex_add(Complex a, Complex b) {
+Complex add_complex(Complex a, Complex b) {
     return (Complex){a.real + b.real, a.imag + b.imag};
 }
 
 // Subtract two complex numbers
-Complex complex_sub(Complex a, Complex b) {
+Complex sub_complex(Complex a, Complex b) {
     return (Complex){a.real - b.real, a.imag - b.imag};
 }
 
 // Multiply two complex numbers
-Complex complex_mul(Complex a, Complex b) {
+Complex mul_complex(Complex a, Complex b) {
     return (Complex){
         a.real * b.real - a.imag * b.imag,
         a.real * b.imag + a.imag * b.real
@@ -24,7 +24,7 @@ Complex complex_mul(Complex a, Complex b) {
 }
 
 // Divide two complex numbers
-Complex complex_div(Complex a, Complex b) {
+Complex div_complex(Complex a, Complex b) {
     double denom = b.real * b.real + b.imag * b.imag;
     return (Complex){
         (a.real * b.real + a.imag * b.imag) / denom,
@@ -33,12 +33,12 @@ Complex complex_div(Complex a, Complex b) {
 }
 
 // Complex conjugate
-Complex complex_conj(Complex z) {
+Complex conj_complex(Complex z) {
     return (Complex){z.real, -z.imag};
 }
 
 // Print complex number
-void complex_print(Complex z) {
+void print_complex(Complex z) {
     printf("%.2f %c %.2fi\n", z.real, (z.imag < 0 ? '-' : '+'), (z.imag < 0 ? -z.imag : z.imag));
 }
 
@@ -46,36 +46,36 @@ int main() {
     Complex z1 = {3.0, 4.0};    // 3 + 4i
     Complex z2 = {1.0, -2.0};   // 1 - 2i
 
-    Complex sum = complex_add(z1, z2);
-    Complex diff = complex_sub(z1, z2);
-    Complex prod = complex_mul(z1, z2);
-    Complex quot = complex_div(z1, z2);
-    Complex conj1 = complex_conj(z1);
-    Complex conj2 = complex_conj(z2);
+    Complex sum = add_complex(z1, z2);
+    Complex diff = sub_complex(z1, z2);
+    Complex prod = mul_complex(z1, z2);
+    Complex quot = div_complex(z1, z2);
+    Complex conj1 = conj_complex(z1);
+    Complex conj2 = conj_complex(z2);
 
     printf("z1 = ");
-    complex_print(z1);
+    print_complex(z1);
 
     printf("z2 = ");
-    complex_print(z2);
+    print_complex(z2);
 
     printf("z1 + z2 = ");
-    complex_print(sum);
+    print_complex(sum);
 
     printf("z1 - z2 = ");
-    complex_print(diff);
+    print_complex(diff);
 
     printf("z1 * z2 = ");
-    complex_print(prod);
+    print_complex(prod);
 
     printf("z1 / z2 = ");
-    complex_print(quot);
+    print_complex(quot);
 
     printf("conj(z1) = ");
-    complex_print(conj1);
+    print_complex(conj1);
 
     printf("conj(z2) = ");
-    complex_print(conj2);
+    print_complex(conj2);
 
     return 0;
 }
